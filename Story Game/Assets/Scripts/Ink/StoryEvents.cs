330581dfb0fb94b560ed8713c74e954c91911ca5
Benjamin Haber <benhasanemail@gmail.com>
2018-11-26 10:15:05 -0500

    moved stuff around, start storyevent handler

---
+++ Story Game/Assets/Scripts/Ink/StoryEvents.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TextBlock
+{
+	public bool IsDialogue;
+	public string Name;
+	public string Text;
+
+}
+
+[Serializable]
+public class BlockUpdateEvent : UnityEvent<TextBlock> {}
+
+public class StoryEvents 
+{
+
+	public static BlockUpdateEvent onBlockUpdate;
+	
+	
+	private void Awake()
+	{
+		onBlockUpdate = new BlockUpdateEvent();
+		
+	}
+	
+}
