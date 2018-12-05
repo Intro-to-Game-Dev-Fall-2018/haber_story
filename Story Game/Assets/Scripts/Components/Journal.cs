8dabd25f209252212c7edb1ca95fb56fc56dd9c8
Benjamin Haber <benhasanemail@gmail.com>
2018-12-05 13:47:04 -0500

    added journal system

--- Story Game/Assets/Scripts/Components/Journal.cs
+++ Story Game/Assets/Scripts/Components/Journal.cs
@@ -1,27 +1,75 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Ink.Runtime;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Journal : MonoBehaviour
 {
+	[Header("Components")]
+	[SerializeField] private Button _openJournal;
+	[SerializeField] private Button _closeJournal;
+	[SerializeField] private CanvasGroup _canvasGroup;
+	[SerializeField] private Transform _content;
 	
-	private Stack<string> _entries;
+	[Header("Prefabs")]
+	[SerializeField] private TextMeshProUGUI _textPrefab;
+	
+	private List<string> _entries;
 	
 	private void Start () {
-		_entries = new Stack<string>();
+		_entries = new List<string>();
 		StoryEvents.i.onBlockUpdate.AddListener(AddText);
 		StoryEvents.i.onChoiceMade.AddListener(AddChoice);
+		
+		_openJournal.onClick.AddListener(OpenJournal);
+		_closeJournal.onClick.AddListener(CloseJournal);
+		HideCanvas();
 	}
 
 	private void AddText(TextBlock block)
 	{
-		_entries.Push(block.Name+": "+block.Text);
+		if (block.IsDialogue)
+			_entries.Add(block.Name+": "+block.Text);
+		else
+			_entries.Add(block.Text);
 	}
 
 	private void AddChoice(Choice choice)
 	{
-		_entries.Push(choice.text);
+		_entries.Add(choice.text);
+	}
+
+	private void OpenJournal()
+	{
+		foreach (string s in _entries)
+		{
+			TextMeshProUGUI tmp = Instantiate(_textPrefab, _content);
+			tmp.text = s;
+		}
+		ShowCanvas();
+	}
+
+	private void CloseJournal()
+	{
+		foreach (Transform child in _content)
+			Destroy(child.gameObject);	
+		
+		HideCanvas();
+	}
+	
+	private void HideCanvas()
+	{
+		_canvasGroup.alpha = 0f;
+		_canvasGroup.blocksRaycasts = false;
+		_canvasGroup.interactable = false;
+	}
+
+	private void ShowCanvas()
+	{
+		_canvasGroup.alpha = 1f;
+		_canvasGroup.blocksRaycasts = true;
+		_canvasGroup.interactable = true;
 	}
 	
 }
--- Story Game/Assets/Scripts/Components/TopMenu.cs
+++ Story Game/Assets/Scripts/Components/TopMenu.cs
@@ -4,7 +4,6 @@ using UnityEngine.UI;
 public class TopMenu : MonoBehaviour
 {
 	[SerializeField] private Button _menu;
-	[SerializeField] private Button _journal;
 	
 	private void Start () {
 		_menu.onClick.AddListener(Menu);
